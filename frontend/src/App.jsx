e2f5e0c8069eb89ee93a3dfb0513dbd140c7d44d
HiteshMehra007 <hiteshmehra809@gmail.com>
2024-06-19 01:05:25 +0530

    updated frontend and added new assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
-import Body  from './components/Body';
+
+import HomePage from "./components/Home/HomePage.jsx";
+import LoginPage from "./components/auth/Login/LoginPage.jsx";
+import SignupPage from "./components/auth/Signup/SignupPage.jsx";
+
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <div className='app'>
-      <Body />
+    <div className='app flex max-w-6xl mx-auto'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,5 +1,17 @@
 import twitter from "../assets/twitter.png";
+import boy1 from "../assets/boy1.png";
+import boy2 from "../assets/boy2.png";
+import boy3 from "../assets/boy3.png";
+import girl1 from "../assets/girl1.png";
+import girl2 from "../assets/girl2.png";
+import girl3 from "../assets/girl3.png";
 
 export default {
     twitter,
+    boy1,
+    boy2,
+    boy3,
+    girl1,
+    girl2,
+    girl3,
 };
