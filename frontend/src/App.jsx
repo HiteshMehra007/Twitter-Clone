77cfc7d259b22e3e4a1515fc0bb4e4be4a3f9d6d
HiteshMehra007 <hiteshmehra809@gmail.com>
2024-06-20 00:22:54 +0530

    complete UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,26 @@ import { useState } from 'react'
 import HomePage from "./components/Home/HomePage.jsx";
 import LoginPage from "./components/auth/Login/LoginPage.jsx";
 import SignupPage from "./components/auth/Signup/SignupPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from './components/common/RightPanel.jsx';
+import NotificationPage from './components/notification/NotificationPage.jsx';
+import ProfilePage from './components/profile/ProfilePage.jsx';
 
 import { Route, Routes } from 'react-router-dom';
-
 
 function App() {
 
   return (
     <div className='app flex max-w-6xl mx-auto'>
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   )
 }
--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -5,6 +5,10 @@ import boy3 from "../assets/boy3.png";
 import girl1 from "../assets/girl1.png";
 import girl2 from "../assets/girl2.png";
 import girl3 from "../assets/girl3.png";
+import post1 from "../assets/post1.png";
+import post2 from "../assets/post2.png";
+import post3 from "../assets/post3.png";
+import cover from "../assets/cover.png";
 
 export default {
     twitter,
@@ -14,4 +18,8 @@ export default {
     girl1,
     girl2,
     girl3,
+    post1,
+    post2,
+    post3,
+    cover,
 };
