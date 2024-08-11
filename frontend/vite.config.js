78b79d812dfb106b9a3aa3fd2674ff07938c168d
HiteshMehra007 <hiteshmehra809@gmail.com>
2024-08-11 19:43:09 +0530

    updated api endpoint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: { 
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://social-media-thzy.onrender.com",
         changeOrigin: true,
       },
     }
